911600cb33d33ce3098a2e3e149af264c1f40f9a
jamtechdev <jamtechdev@gmail.com>
2024-07-18 13:54:28 +0530

    port change

--- index.js
+++ index.js
@@ -14,12 +14,15 @@ app.post("/webhook/incoming", async (req, res) => {
   const data = req.body;
   //   console.log(data[0])
   // console.log(data);
-
-  const username = data[0].contact.username;
-  const lastMessage = data[0].contact.last_message;
-  console.log("Username:", username);
-  console.log("Last Message:", lastMessage);
-  return res.send().status(200);
+  try {
+    const username = data[0]?.contact.username;
+    const lastMessage = data[0]?.contact.last_message;
+    console.log("Username:", username);
+    console.log("Last Message:", lastMessage);
+    return res.send().status(200);
+  } catch (error) {
+    res.status(500).send();
+  }
 
   // Check if the username and last message exist in the Supabase database
   const { data: user, error } = await supabase

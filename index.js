545a3359fa576b2b8cb7acbf4572483cb3e5d373
jamtechdev <jamtechdev@gmail.com>
2024-07-18 11:28:24 +0530

    port change

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post("/webhook/outgoing", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8090;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

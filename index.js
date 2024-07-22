546e8fc4bf64a1feb66250fdfb2fcd090d1a2216
jamtechdev <jamtechdev@gmail.com>
2024-07-22 15:32:49 +0530

    ots

--- index.js
+++ index.js
@@ -62,19 +62,18 @@ const getNewMessages = async () => {
 
   threads.forEach((thread) => {
     thread.items.forEach((message) => {
-      console.log(message);
-      // if (!processedMessageIds.has(message.item_id)) {
-      //   // Process the new message
-      //   console.log("New message:", message.media_type);
-      //   console.log("New message:", processedMessageIds);
+      if (!processedMessageIds.has(message.item_id)) {
+        // Process the new message
+        console.log("New message:", message.item_type);
+        console.log(message.item_type === "media_share");
 
-      //   // Add the message ID to the set of processed IDs
-      //   processedMessageIds.add(message.item_id);
+        // Add the message ID to the set of processed IDs
+        processedMessageIds.add(message.item_id);
 
-      //   // Save the processed message ID to storage (this is just an example)
-      //   // In a real application, you would save this to a database or file
-      //   // saveProcessedMessageIdToStorage(message.item_id);
-      // }
+        // Save the processed message ID to storage (this is just an example)
+        // In a real application, you would save this to a database or file
+        // saveProcessedMessageIdToStorage(message.item_id);
+      }
     });
   });
 };
